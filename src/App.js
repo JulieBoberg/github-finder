04d71544dcc2769a761cd523c2d834508062c0b6
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2019-11-19 12:44:51 -0800

    Added About page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
    async componentDidMount() {
@@ -31,16 +35,35 @@ class App extends Component {
 
   clearUsers=()=> this.setState({users:[], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({alert: { msg, type } });
+
+    setTimeout(()=>this.setState({alert: null}), 5000);
+  }
+
   render() {
     const {users, loading} = this.state;
+
     return (
+      <Router>
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false} />
+          <Alert alert={this.state.alert}/>
+          <Switch>
+            <Route exact path='/' render={props=>(
+              <Fragment>
+              <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false} setAlert={this.setAlert} />
           <Users loading={loading} users = {users} />
+
+              </Fragment> 
+            )} />
+            <Route exact path ='/about' component={About} />
+          </Switch>
+          
         </div>
       </div>
+      </Router>
     );
   }
 }
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 export class Search extends Component {
-    state = {
-        text:''
-    };
+  state = {
+    text: ""
+  };
 
-    static propTypes={
-        searchUsers:PropTypes.func.isRequired,
-        clearUsers:PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-    };
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
 
-    onChange = (e) =>
-        this.setState({ [e.target.name]: e.target.value });
-    
-    onSubmit=(e)=>{
-        e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text:''})
-    };
-    
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
+  };
 
   render() {
-      const {showClear, clearUsers} = this.props;
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
-          <input type='text' name='text' placeholder='Search User...' value={this.state.text} onChange={this.onChange} />
+          <input
+            type='text'
+            name='text'
+            placeholder='Search User...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type='submit'
             value='Search'
@@ -35,7 +44,9 @@ export class Search extends Component {
           />
         </form>
         {showClear && (
-        <button className='btn btn-light btn-block' onClick={clearUsers}>Clear</button>
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
         )}
       </div>
     );
